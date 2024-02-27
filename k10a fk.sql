6abdc7c97646cc9be164b673c46c7e700cdb2301
Iremkeser37 <145453393+Iremkeser37@users.noreply.github.com>
2024-02-27 12:56:44 +0300

    Add files via upload

---
+++ k10a fk.sql
@@ -0,0 +1,41 @@
+-- --------------------------------------------------------
+-- Sunucu:                       127.0.0.1
+-- Sunucu sürümü:                10.4.21-MariaDB - mariadb.org binary distribution
+-- Sunucu İşletim Sistemi:       Win64
+-- HeidiSQL Sürüm:               11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- k10a için veritabanı yapısı dökülüyor
+CREATE DATABASE IF NOT EXISTS `k10a` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `k10a`;
+
+-- tablo yapısı dökülüyor k10a.kitap
+CREATE TABLE IF NOT EXISTS `kitap` (
+  `sira` int(11) NOT NULL AUTO_INCREMENT,
+  `ad` varchar(50) NOT NULL DEFAULT '0',
+  `yazar sira` int(11) NOT NULL DEFAULT 0,
+  `tur sira` int(11) NOT NULL DEFAULT 0,
+  `sayfasayisi` smallint(6) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`sira`),
+  KEY `FK_kitap_yazar` (`yazar sira`),
+  KEY `FK_kitap_tur` (`tur sira`),
+  CONSTRAINT `FK_kitap_tur` FOREIGN KEY (`tur sira`) REFERENCES `tur` (`sira`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_kitap_yazar` FOREIGN KEY (`yazar sira`) REFERENCES `yazar` (`sira`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- k10a.kitap: ~0 rows (yaklaşık) tablosu için veriler indiriliyor
+/*!40000 ALTER TABLE `kitap` DISABLE KEYS */;
+/*!40000 ALTER TABLE `kitap` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
